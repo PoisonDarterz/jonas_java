27ed3a257084201dc18c5f1949f0225db60d6cbb
PoisonDarterz <jc.chuan.0303@gmail.com>
2022-03-22 23:32:08 +0800

    lab5.2 complete, lab5.3 q1

--- src/lab5_2.java
+++ src/lab5_2.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class q52_1 {
     public static void main(String[] args) {
         for (int i = 1; i <=30 ; i+=2){
@@ -9,15 +11,62 @@ class q52_1 {
 class q52_2{
     public static void main(String[] args) {
         int sum =0;
-        for (int i = 1; i <=10 ; i++){
-            sum = sum + i;
-        }
+            for (int i = 1; i <=10 ; i++){
+                sum = sum + i;
+            }
         System.out.println("Sum of first 10 natural numbers is: " + sum);
     }
 }
 
 class q52_3{
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int i;
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.println("-----Multiplication Table-----");
+            for(i=1; i<=12; i++){
+                System.out.println(i + " x " + num + " = " + (i*num));
+            }
+    }
+}
+
+
+class q52_4{
+    public static void main(String[] args) {
+        for (int i = 100; i < 200; i+=10){
+            System.out.print("\n");
+            for(int j = 0; j<10; j++){
+                if(i+j<=200){
+                    System.out.print((i+j) + " ");
+                }
+            }
+        }
+        System.out.println("\n200");
+    }
+}
+
+
+class q52_5{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of items: ");
+        int qty = sc.nextInt();
+        int price,min=9999999,max=-9999999,ttl=0;
+            for(int i=0; i<qty; i++){
+                System.out.println("Enter price of item: ");
+                price = sc.nextInt();
+
+                if(price < min){
+                    min = price;
+                } else if (price > max){
+                    max = price;
+                }
+
+                ttl = ttl + price;
+            }
+        System.out.println("Total is: " + ttl);
+        System.out.println("Min is: " + min);
+        System.out.println("Max is: " + max);
     }
 }
---
+++ src/lab5_3.java
@@ -0,0 +1,20 @@
+class q53_1 {
+    public static void main(String[] args) {
+        int i = 1;
+        int sum = 0;
+        while (sum < 100){
+            sum = sum + i;
+            i = i + 1;
+            
+            if(sum<100){
+                System.out.println(sum);
+            }
+        }
+    }
+}
+
+class q53_2{
+    public static void main(String[] args) {
+        
+    }
+}
