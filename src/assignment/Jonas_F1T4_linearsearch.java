ab0d02bd2d1a28888eadbcb95e2b8e0a59c4e087
PoisonDarterz <jc.chuan.0303@gmail.com>
2022-04-09 18:57:21 +0800

    code formatting

--- src/assignment/Jonas_F1T4_linearsearch.java
+++ src/assignment/Jonas_F1T4_linearsearch.java
@@ -10,28 +10,30 @@ import java.util.Scanner;
 import java.text.DecimalFormat;
 
 class jonas {
+    //declare static objects to be able to use in all methods
     static Scanner sc = new Scanner(System.in);
     static DecimalFormat df = new DecimalFormat("#.00");
     public static void main(String[] args) {
-        jonas obj = new jonas();
-
+        jonas obj = new jonas(); //class object
+        //initialisation of arrays required
         String[] stationID = new String[5];
         String[] stationName = new String[5];
         double[] fahrenheit = new double[5];
         double[] celsius = new double[5];
 
+        //obtain inputs via methods, and conversion of F to C temperatures
         stationID = obj.getID();
         stationName = obj.getName(stationID);
         fahrenheit = obj.getTemp(stationName);
         celsius = obj.getCelsius(fahrenheit);
-
+        //calculate average
         double average = obj.getAvg(celsius);
         String dfavg = df.format(average);
 
+            //determine max and min rows
             double maxTemp=celsius[0],minTemp=celsius[0];
             String maxID=stationID[0],maxName=stationName[0],minID=stationID[0],minName=stationName[0];
             int maxIndex=0,minIndex=-1;
-
             for(int m = 0; m<5; m++){
                 if(celsius[m] > maxTemp){
                     maxTemp = celsius[m];
@@ -49,53 +51,56 @@ class jonas {
                 }
             }
 
+        //final report, and search
         obj.tempReport(maxTemp,minTemp,maxID,minID,maxIndex,minIndex,maxName,minName,dfavg);
         obj.search(stationID,stationName,celsius);
-    }
+    } //end main()
 
+    //input methods getID(), getName(), getTemp()
     String[] getID(){
         System.out.println("-------Input of Station ID--------");
         String[] input = new String[5];
-        for(int i=0; i<5; i++){
-            System.out.print("Enter station ID " + (i+1) + ": ");
-            input[i] = sc.nextLine();
-        }
+            for(int i=0; i<5; i++){
+                System.out.print("Enter station ID " + (i+1) + ": ");
+                input[i] = sc.nextLine();
+            }
         return input;
     }
 
     String[] getName(String[] sID){
         System.out.println("\n-------Input of Station Name-------");
         String[] input = new String[5];
-        for(int i=0; i<5; i++){
-            System.out.print("Enter station name for " + sID[i] + ": ");
-            input[i] = sc.nextLine();
-        }
+            for(int i=0; i<5; i++){
+                System.out.print("Enter station name for " + sID[i] + ": ");
+                input[i] = sc.nextLine();
+            }
         return input;
     }
 
     double[] getTemp(String[] sName){
         System.out.println("\n-----Input of Temperature in Fahrenheit-----");
         double[] input = new double[5];
-        for(int i=0; i<5; i++){
-            System.out.print("Enter temperature of " + sName[i] + ": ");
-            input[i] = sc.nextDouble();
-        }
+            for(int i=0; i<5; i++){
+                System.out.print("Enter temperature of " + sName[i] + ": ");
+                input[i] = sc.nextDouble();
+            }
         return input;
     }
 
+    //calculation method getCelsius(), getAvg()
     double[] getCelsius(double[] frh){
         double[] cels = new double[5];
-        for(int i=0; i<5; i++){
-            cels[i] = (frh[i] - 32) * 5 / 9;
-        }
+            for(int i=0; i<5; i++){
+                cels[i] = (frh[i] - 32) * 5 / 9;
+            }
         return cels;
     }
 
     double getAvg(double[] cels) {
         double avg,sum=0;
-        for(int i=0; i<5; i++){
-            sum = sum + cels[i];
-        }
+            for(int i=0; i<5; i++){
+                sum = sum + cels[i];
+            }
         avg = sum / 5;
         return avg;
     }
@@ -103,22 +108,23 @@ class jonas {
     void tempReport(double maxTemp, double minTemp, String maxID, String minID,
                     int maxIndex, int minIndex, String maxName, String minName, String dfavg)
     {
+        //formatting decimal places
         String dfmax = df.format(maxTemp);
         String dfmin = df.format(minTemp);
-
+        //outputs
         System.out.println("\n---------------Temperature Report------------------");
         System.out.println("Average temperature is: " + dfavg + " Celsius.");
         System.out.println("Type   \t" + "    Index     ID        Name        Temperature");
         System.out.println("Maximum: \t" + maxIndex + "\t" + maxID + "\t" + maxName + "\t" + dfmax + " Celsius.");
         System.out.println("Minimum: \t" + minIndex + "\t" + minID + "\t" + minName + "\t" + dfmin + " Celsius.");
-    }
+    } //end tempReport()
 
     void search(String[] stationID, String[] stationName, double[] celsius){
         System.out.println("\n======= Station ID Search =======");
         String yesno="";
         String target;
         do{
-            do{
+            do{ //this loop verifies that user wants to search. if "N" is inputted, the program will end immediately.
                 System.out.print("Search by station ID? (Y/N): ");
                 yesno = sc.next();
                     if (yesno.equals("N")){
@@ -130,30 +136,30 @@ class jonas {
                         System.out.println("Invalid input!");
                     }
             } while(!(yesno.equals("Y") || yesno.equals("N")));
-            System.out.print("Enter station ID to search: ");
-            target = sc.next();
+            //var initialisations
             int tgtIndex;
             String tgtID,tgtName;
             double tgtTemp;
-            boolean found = false;
-            for(int n =0; n<5; n++){
-                if(stationID[n].equals(target)){
-                    tgtTemp = celsius[n];
-                    tgtID = stationID[n];
-                    tgtName = stationName[n];
-                    tgtIndex = n;
-                    found = true;
-                    String dftgtTemp = df.format(tgtTemp);
-                    System.out.println("Target found.");
-                    System.out.println("Index  ID        Name        Temperature");
-                    System.out.println(tgtIndex + "\t" + tgtID + "\t" + tgtName + "\t" + dftgtTemp + " Celsius.");
+            //search algorithm
+            System.out.print("Enter station ID to search: ");
+                target = sc.next();
+                boolean found = false;
+                for(int n =0; n<5; n++){
+                    if(stationID[n].equals(target)){
+                        tgtTemp = celsius[n];
+                        tgtID = stationID[n];
+                        tgtName = stationName[n];
+                        tgtIndex = n;
+                        found = true;
+                        String dftgtTemp = df.format(tgtTemp);
+                        System.out.println("Target found.");
+                        System.out.println("Index  ID        Name        Temperature");
+                        System.out.println(tgtIndex + "\t" + tgtID + "\t" + tgtName + "\t" + dftgtTemp + " Celsius.");
+                    }
                 }
-            }
-
-            if(!found){
-                System.out.println("No record found. Please search again.");
-            }
-        } while(true);
-
-    }
+                if(!found){
+                    System.out.println("No record found. Please search again.");
+                }
+        } while(true); //loops back to line 126
+    } //end search()
 }
