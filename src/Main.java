4a21e6df0cd27f5ff76eb8aa117972376f25ef1a
PoisonDarterz <jc.chuan.0303@gmail.com>
2022-11-08 17:17:34 +0800

    random

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        // String x, lower;
+        // char arr[];
+
+        // for (int i = 0; i < 2; i++){
+        //     x = sc.nextLine();
+        //     lower = x.toLowerCase();
+        //     arr = lower.toCharArray();
+        //     Arrays.sort(arr);
+        //     System.out.println(arr);
+        // }
+        int year;
+        while (true){
+            year = sc.nextInt();
+            boolean leap = ( (year % 4 == 0 && year % 100 !=0) || year % 400 == 0) ? true : false;
+            System.out.println(leap);
+        }
+
+        
     }
 }
